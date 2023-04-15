abfe05d5ac9f294086b0297773140f2efb82d0f2
haris <m.haris8790@gmail.com>
2023-04-15 17:06:51 +0500

    Lambda function integrated

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require('express')
 const cors = require('cors')
 const bodyParser = require('body-parser');
 const app = express()
+const AWS = require("aws-sdk");
 app.use(cors())
 const dotenv = require('dotenv')
 dotenv.config()
@@ -11,19 +12,34 @@ app.use(
   })
 );
 app.use(bodyParser.json());
+
+AWS.config.update({ region: "ap-northeast-1" });
+const lambda = new AWS.Lambda();
+const params = {
+  FunctionName: "Twilio_demo",
+  InvocationType: "RequestResponse",
+};
 const accountSid = process.env.TWILIO_ACCOUNT_SID;
 const authToken = process.env.TWILIO_AUTH_TOKEN;
 const client = require("twilio")(accountSid, authToken);
 app.get('/message/:num',(req,res)=>{
     const userContactNo = req.params.num
     console.log(userContactNo)
-    client.messages
-      .create({
-        body: "MhKing here",
-        from: process.env.MY_PHONE_NUM,
-        to: userContactNo,
-      })
-      .then((message) => res.send(message.sid));
+    lambda.invoke(params, function (err, data) {
+      if (err) console.log(err, err.stack);
+      else {
+        const response = JSON.parse(data.Payload);
+        console.log(response.body.message);
+        client.messages
+          .create({
+            body: response.body.message,
+            from: process.env.MY_PHONE_NUM,
+            to: userContactNo,
+          })
+          .then((message) => res.send(message.sid));
+        
+      }
+    });
 })
 app.listen(process.env.PORT, () =>
   console.log(`Server running on port ${process.env.PORT}`)
